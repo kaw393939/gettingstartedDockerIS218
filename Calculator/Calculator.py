5861bb7f602dc84c27f81cd884167ee9be575a42
kaw393939 <keith@webizly.com>
2020-01-28 15:21:32 -0500

    task: added calculator addition test

---
+++ Calculator/Calculator.py
@@ -0,0 +1,7 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def addition(self, a, b):
+        return a + b
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -1,10 +1,19 @@
 import unittest
 
+from Calculator.Calculator import Calculator
+
 
 class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.calculator, Calculator)
+        calculator = Calculator()
+
+        self.assertIsInstance(calculator, Calculator)
+
+    def test_calculator_addition(self):
+        calculator = Calculator()
+        result = calculator.addition(1, 2)
+        self.assertEquals(3, result)
 
 
 if __name__ == '__main__':
