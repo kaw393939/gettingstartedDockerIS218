14776d936433ca59fc71cdc244a97c341821bed1
kaw393939 <keith@webizly.com>
2020-02-04 14:12:33 -0500

    Lesson: Calculator with Object Example

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -3,12 +3,15 @@ from MathOperations.subtraction import Subtraction
 
 
 class Calculator:
+    Result = 0
 
     def __init__(self):
         pass
 
     def Sum(self, a, b):
-        return Addition.sum(a, b);
+        self.Result = Addition.sum(a, b)
+        return self.Result
 
     def Difference(self, a, b):
-        return Subtraction.difference(a, b)
+        self.Result = Subtraction.difference(a, b)
+        return self.Result
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -9,15 +9,32 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
-    def test_calculator_addition(self):
+    def test_calculator_return_sum(self):
         calculator = Calculator()
         result = calculator.Sum(1, 2)
         self.assertEqual(3, result)
 
-    def test_calculator_subtraction(self):
+    def test_calculator_return_difference(self):
         calculator = Calculator()
         result = calculator.Difference(1, 2)
         self.assertEqual(-1, result)
+
+    def test_calculator_access_difference_result(self):
+        calculator = Calculator()
+        calculator.Difference(1, 2)
+        self.assertEqual(-1, calculator.Result)
+
+    def test_calculator_access_sum_result(self):
+        calculator = Calculator()
+        calculator.Sum(1, 2)
+        self.assertEqual(3, calculator.Result)
+
+    def test_multiple_calculators(self):
+        calculator1 = Calculator()
+        calculator2 = Calculator()
+        calculator3 = Calculator()
+        calculator3.Sum(calculator1.Sum(1, 2), calculator2.Difference(3, 4))
+        self.assertEqual(2, calculator3.Result)
 
 
 if __name__ == '__main__':
